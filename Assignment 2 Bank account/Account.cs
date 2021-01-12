2149ec8acc3f4855405847014886e329143a144a
Preetmohan Manchanda <preetmohansm@outlook.com>
2021-01-12 14:58:36 +0700

    Add project files.

---
+++ Assignment 2 Bank account/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2_Bank_account
+{
+    class Account
+    {
+        private string name;
+        private double balance;
+
+        public Account(string name, double balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public double CheckBalance()
+        {
+            return balance;
+        }
+        public double DepositMoney(double MoneyIn)
+        {
+            if(MoneyIn> 0)
+            {
+                Console.WriteLine("Money added successfully!");
+                balance = balance + MoneyIn;
+            }
+            else
+            {
+                Console.WriteLine("Scam attempt");
+            }
+            return balance;
+
+        }
+        public double WithdrawMoney(double MoneyOut)
+        {
+            if (MoneyOut > balance)
+            {
+                Console.WriteLine("You do not have enough money");
+            }
+            else if(MoneyOut > 0)
+            {
+                Console.WriteLine("Withdraw successful!");
+                balance = balance - MoneyOut;
+            }
+            else
+            {
+                Console.WriteLine("Dont try and scam us with negative numbers!");  
+            }
+            return balance;
+
+        }
+        // Interest rate is in the format 1.05
+        //This means 5%
+        public double AddInterest(double InterestRate)
+        {
+            string date = DateTime.Now.ToShortDateString();
+            Console.WriteLine(date);
+            if(date == "01/01/2021")
+            {
+               balance = balance * InterestRate;
+            }
+        
+            return balance;
+        } 
+    }
+}
+
---
+++ Assignment 2 Bank account/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment_2_Bank_account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account Preet = new Account("Preet", 100);
+            Console.WriteLine(Preet.AddInterest(1.05));
+        
+        }
+    }
+}
